0be8df870d916e0d96402eea82ede6bfe23ed678
Nikhil Singh <m.j882600@gmail.com>
2025-07-04 15:27:21 +0530

    feat: configure CORS to allow requests from deployed frontend only

--- server.js
+++ server.js
@@ -2,7 +2,12 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://eatzyy.netlify.app",
+    optionsSuccessStatus: 200,
+  })
+);
 
 app.get(/^\/api\/(.*)/, async (req, res) => {
   try {
